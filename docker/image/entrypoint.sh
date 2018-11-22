fcd0630d857e9176957528c266e35913ff306fd5
cghislai <charlyghislain@gmail.com>
2018-11-22 22:50:02 +0200

    Docker fixes....

--- docker/image/entrypoint.sh
+++ docker/image/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 PRE_BOOT_FILE="/opt/payara/pre-boot-commands.sh"
-POST_BOOT_FILE="/opt/payara/post-boot-commands.sh"
 
 DB_DRIVER="${DB_DRIVER:-mariadb}"
 DB_USER="${DB_USER:-}"
@@ -10,20 +9,8 @@ DB_PASSWORD_FILE="${DB_PASSWORD_FILE:-/var/run/secrets/db-password}"
 DB_NAME="${DB_NAME:-}"
 DB_SERVER="${DB_SERVER:-}"
 
-CONFIG_NAME="${CONFIG_NAME:-Authenticator}"
-CONFIG_URL="${CONFIG_URL:-}"
-CONFIG_TOKEN_ISSUER="${CONFIG_TOKEN_ISSUER:-}"
-CONFIG_DEFAULT_ADMIN_MAIL="${CONFIG_DEFAULT_ADMIN_MAIL:-}"
-CONFIG_CORS_ALLOWED_ORIGINS="${CONFIG_CORS_ALLOWED_ORIGINS:-*}"
-
 if [[ -f "$DB_PASSWORD_FILE" && -z "$DB_PASSWORD" ]] ; then
     DB_PASSWORD="$(head -n1 ${DB_PASSWORD_FILE})"
-fi
-if [[ -z "$CONFIG_URL" ]] ; then
-    echo "No CONFIG_URL provided" && exit 1
-fi
-if [[ -z "CONFIG_TOKEN_ISSUER" ]] ; then
-    echo "No CONFIG_TOKEN_ISSUER provided" && exit 1
 fi
 
 cat << EOF > ${PRE_BOOT_FILE}
@@ -32,14 +19,6 @@ set configs.config.server-config.admin-service.das-config.dynamic-reload-enabled
 # Https only, even with an expired self-signed
 set configs.config.server-config.network-config.network-listeners.network-listener.https-listener.enabled=true
 set configs.config.server-config.network-config.network-listeners.network-listener.http-listener.enabled=false
-EOF
-
-cat << EOF > ${POST_BOOT_FILE}
-set-config-property --source=domain --propertyValue="${CONFIG_NAME}" --propertyName="payara.microprofile.com.charlyghislain.authenticator.name"
-set-config-property --source=domain --propertyValue="${CONFIG_URL}" --propertyName="payara.microprofile.com.charlyghislain.authenticator.url"
-set-config-property --source=domain --propertyValue="${CONFIG_TOKEN_ISSUER}" --propertyName="payara.microprofile.com.charlyghislain.authenticator.token.issuer"
-set-config-property --source=domain --propertyValue="${CONFIG_DEFAULT_ADMIN_MAIL}" --propertyName="payara.microprofile.com.charlyghislain.authenticator.admin.defaultEmail"
-set-config-property --source=domain --propertyValue="${CONFIG_CORS_ALLOWED_ORIGINS}" --propertyName="payara.microprofile.com.charlyghislain.authenticator.cors.allowedOrigins"
 EOF
 
 if [[ "$DB_DRIVER" == "mariadb" ]] ; then
@@ -69,6 +48,5 @@ fi
 
 exec "java" "-jar" "/opt/payara/payara-micro.jar" \
     "--prebootcommandfile" "${PRE_BOOT_FILE}" \
-    "--postbootcommandfile" "${POST_BOOT_FILE}" \
     "--addlibs" "/opt/payara/lib/" \
     "$@"
